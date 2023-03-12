f1f474b6963a514f7504535567e7033c240c7826
Vladyslav Lobyntsev <vlobyntsev@deloitte.com>
2023-03-12 23:18:22 +0300

    added background execution

--- lib/core/refreshing_job.ex
+++ lib/core/refreshing_job.ex
@@ -1,20 +1,38 @@
 defmodule Core.RefreshingJob do
   use GenServer
 
+  defstruct timeout: 6000, callback: nil, rate: nil
+
   def start_link(args) do
+
     GenServer.start_link(__MODULE__, args, [])
   end
 
   def init(args) do
+    schedule_next(args)
+
     {:ok, args}
   end
 
-  def handle_call(:get_rates, _from, args) do
-    new_args = case args do
-      [{:rate, _ }| _] -> args
-      [callback: callback] -> [rate: callback.()] ++ args
-    end
-    {:reply, new_args[:rate], new_args}
+  def handle_call(:get_rates, from, %__MODULE__{rate: nil, callback: callback} = args) do
+
+    handle_call(:get_rates, from, %__MODULE__{args | rate: callback.()})
+  end
+
+  def handle_call(:get_rates, _, %{rate: rate} = args) do
+
+    {:reply, rate, args}
+  end
+
+  def handle_info(:poll, %{callback: callback} = args) do
+    schedule_next(args)
+
+    {:noreply, Map.put(args, :rate, callback.())}
+  end
+
+  defp schedule_next(%{timeout: timeout}) do
+
+    Process.send_after(self(), :poll, timeout)
   end
 
 end
